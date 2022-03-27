c06e8665ed7d2875e1439d5ec58016a13caafb08
王心凌 <maple@lihuia.com>
2022-03-27 15:23:19 +0800

    add modules

---
+++ mysterious-starter/src/main/java/com/lihuia/mysterious/starter/MysteriousApplication.java
@@ -0,0 +1,22 @@
+package com.lihuia.mysterious.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lihuia.com
+ * @date 2022/3/27 1:07 PM
+ */
+
+@SpringBootApplication
+public class MysteriousApplication {
+
+    public static void main(String[] args) {
+        try {
+            SpringApplication.run(MysteriousApplication.class, args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("-----------Mysterious-----------");
+    }
+}
