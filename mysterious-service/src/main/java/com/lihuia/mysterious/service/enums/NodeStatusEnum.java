c2d0d3eec9adc95c360edb7877bb15c4820a5fc9
lihui <maple@lihuia.com>
2022-03-27 23:11:00 +0800

    update all modules

---
+++ mysterious-service/src/main/java/com/lihuia/mysterious/service/enums/NodeStatusEnum.java
@@ -0,0 +1,29 @@
+package com.lihuia.mysterious.service.enums;
+
+/**
+ * @author lihuia.com
+ * @date 2022/3/27 10:58 PM
+ */
+
+public enum NodeStatusEnum {
+
+    DISABLED(0, "禁用中"),
+    ENABLE(1, "启用中"),
+    FAILED(2, "启用失败");
+
+    private Integer code;
+    private String description;
+
+    NodeStatusEnum(Integer code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
