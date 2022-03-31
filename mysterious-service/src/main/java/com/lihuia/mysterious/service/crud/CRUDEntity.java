188dd143e28d086682ddccae3436a2d31fcb588c
lihui <maple@lihuia.com>
2022-04-01 00:05:19 +0800

    add和update操作，更新人和时间信息，封装

--- mysterious-service/src/main/java/com/lihuia/mysterious/service/crud/CRUDEntity.java
+++ mysterious-service/src/main/java/com/lihuia/mysterious/service/crud/CRUDEntity.java
@@ -14,6 +14,11 @@ import java.time.LocalDateTime;
 @Component
 public class CRUDEntity<T extends BaseDO> {
 
+    /**
+     * 每当做add操作，更新创建和更新者信息，每次更新者信息也刷新是因为分页会按照时间排序，因此不能为空
+     * @param t
+     * @param userVO
+     */
     public void addT(T t, UserVO userVO) {
         t.setCreator(userVO.getUsername());
         t.setCreatorId(userVO.getId());
@@ -23,6 +28,11 @@ public class CRUDEntity<T extends BaseDO> {
         t.setModifyTime(LocalDateTime.now());
     }
 
+    /**
+     * 没当做update操作，更新更新着信息
+     * @param t
+     * @param userVO
+     */
     public void updateT(T t, UserVO userVO) {
         t.setId(userVO.getId());
         t.setModifier(userVO.getUsername());
