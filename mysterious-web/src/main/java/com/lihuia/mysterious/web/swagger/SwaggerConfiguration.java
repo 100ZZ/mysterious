362d0107534f118d8ff700828c43d76722aa0ec1
lihui <maple@lihuia.com>
2022-04-17 18:14:55 +0800

    Swagger配置，新增Header token

--- mysterious-web/src/main/java/com/lihuia/mysterious/web/swagger/SwaggerConfiguration.java
+++ mysterious-web/src/main/java/com/lihuia/mysterious/web/swagger/SwaggerConfiguration.java
@@ -3,13 +3,19 @@ package com.lihuia.mysterious.web.swagger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author lihuia.com
@@ -22,6 +28,16 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket createRestApi() {
+        ParameterBuilder tokenParameter = new ParameterBuilder();
+        List<Parameter> parameterList = new ArrayList<>();
+        tokenParameter
+                .name("token")
+                .description("用户token认证")
+                .modelRef(new ModelRef("string"))
+                .parameterType("header")
+                .required(false).build();
+        parameterList.add(tokenParameter.build());
+
         return new Docket(DocumentationType.SWAGGER_2)
                 // 指定构建api文档的详细信息的方法：apiInfo()
                 .apiInfo(apiInfo())
@@ -32,7 +48,7 @@ public class SwaggerConfiguration {
                 //.apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 //可以根据url路径设置哪些请求加入文档，忽略哪些请求
-                .build();
+                .build().globalOperationParameters(parameterList).apiInfo(apiInfo());
     }
 
     /**
