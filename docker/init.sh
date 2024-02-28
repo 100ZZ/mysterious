62b136e8485e12b5b7eac8caef24b1b714a7a526
王心凌 <maple@lihuia.com>
2024-02-28 14:09:12 +0800

    update init.sh

--- docker/init.sh
+++ docker/init.sh
@@ -9,23 +9,23 @@ fi
 
 
 mkdir $BASE_DIR
-cd $BASE_DIR
 
 # 创建mysql目录
-mkdir -p mysql/sql
+mkdir -p ${BASE_DIR}/mysql/sql
 cp schema.sql ${BASE_DIR}/mysql/sql/
 cp insert.sql ${BASE_DIR}/mysql/sql/
-mkdir -p mysql/data
+mkdir -p ${BASE_DIR}/mysql/data
 
 # 创建nginx目录
-mkdir -p nginx/conf.d
+mkdir -p ${BASE_DIR}/nginx/conf.d
 cp 1234.conf ${BASE_DIR}/nginx/conf.d/
-mkdir -p nginx/html
+mkdir -p ${BASE_DIR}/nginx/html
 
 # 创建压测数据，报告目录
-mkdir -p mysterious-data
+mkdir -p ${BASE_DIR}/mysterious-data
 
 # 创建jmeter工具包目录
+cd ${BASE_DIR}
 git clone https://github.com/100ZZ/mysterious-jmeter.git
 rm -rf mysterious-jmeter/.git*
 
