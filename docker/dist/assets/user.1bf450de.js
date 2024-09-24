baa96bca4b9d300e8196c5e524e07c5db386f1d0
lihui <maple@lihuia.com>
2024-09-24 11:09:40 +0800

    update docker dist

--- docker/dist/assets/user.1bf450de.js
+++
@@ -1,1 +0,0 @@
-import{s}from"./request.d7d01389.js";const e=r=>s({url:"/user/login",method:"post",data:r}),a=r=>s({url:"/user/add",method:"post",data:r}),o=r=>s({url:"/user/list",method:"get",params:r});export{a,o as g,e as l};
