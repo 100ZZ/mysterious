1d8a428fc109b2f12fc26a523334818719dfd932
lihui <maple@lihuia.com>
2024-10-12 23:42:30 +0800

    优化csv操作

---
+++ docker/dist/assets/user.920cd2fb.js
@@ -0,0 +1,1 @@
+import{s}from"./request.e8da4d31.js";const e=r=>s({url:"/user/login",method:"post",data:r}),a=r=>s({url:"/user/add",method:"post",data:r}),o=r=>s({url:"/user/list",method:"get",params:r});export{a,o as g,e as l};
